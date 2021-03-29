972bcad64ccc8bb303b5906796d9eb2752130b65
lebahoang <hoangleba@gmail.com>
2021-03-30 02:01:18 +0700

    add

--- gcj/2021/qualification/3.cpp
+++ gcj/2021/qualification/3.cpp
@@ -12,11 +12,11 @@
 using namespace std;
 int calculate(vector<int> a) {
     int total = 0;
-    int n = a.size();
-    for (int i = 0; i < n-1; i++) {
+    int n = a.size()-1;
+    for (int i = 1; i < n; i++) {
         int minV = 100000;
         int minInd = -1;
-        for (int j = i; j < n; j++) {
+        for (int j = i; j <= n; j++) {
             if (a[j] < minV) {
                 minV = a[j];
                 minInd = j;
@@ -34,40 +34,67 @@ int calculate(vector<int> a) {
     }
     return total;
 }
-void dfs(int n, int c, int i, int total, vector<int>& x, vector<int>& ans) {
-    if (i == n-1) {
-        if (total == c) {
-            ans = vector<int>(x.begin(), x.end());
-        }
-        return;
-    }
-    for (int k = 1; k <= n-i+1; k++) {
-        x.push_back(k);
-        if(total+k > c) {
-            x.pop_back();
-            return;
-        }
-        dfs(n,c,i+1,total+k,x,ans);
-        x.pop_back();
-        if ((int)ans.size() > 0) return;
-    }
-}
+
 string solve(int n, int c) {
-    vector<int> x;
-    vector<int> ans;
-    for (int k = 1; k <= n; k++) {
-        x.push_back(k);
-        dfs(n,c,1,k,x,ans);
-        x.pop_back();
-        if ((int)ans.size() > 0) {
-            string ss = "";
-            for (int x: ans) {
-                ss += " " + to_string(x);
-            }
-            return ss;
-        }
+    if (c < n-1 || c > (n*(n+1)/2)) {
+        return " IMPOSSIBLE";
     } 
-    return " IMPOSSIBLE";
+    if (c == n-1) {
+        string x = "";
+        for (int i = 1; i <= n; i++) x += " " + to_string(i);
+        return x;
+    }
+    if (c == n) {
+        string x = " 2 1";
+        for (int i = 3; i <= n; i++) {
+            x += " " + to_string(i);
+        }
+        return x;
+    }
+
+    int nn = n;
+    vector<int> rs(n+1, 0);
+    rs[1] = n;
+    vector<vector<int>> step(n+1, vector<int>(n+1, 0));
+    step[1][1] = n;
+    step[2][1] = 1;
+    int place = c-n-(n-3);
+    c -= n;
+    for (int i = 2; i < nn; i++) {
+        step[i][i] = i+place-1;
+        for (int j = i-1; j >= 1; j--) {
+            step[j][i] = step[j][i-1]-(step[j+1][i]-step[j+1][i-1]);
+        }
+        printf("step %d\n", i);
+        for (int j = 1; j <= i-1; j++) {
+            vector<int> t(n+1, 0);
+            for (int k = 1; k <= nn; k++) {
+                t[step[j][k]] = k;
+            }
+            for (int k = 1; k <= nn; k++) {
+                printf("%d ", t[k]);
+            }
+            printf("\n");
+        }
+        printf("----------------\n");
+        step[i+1][i] = i;
+        n--;
+        place = c-n-(n-3);
+        c -= n;
+    }
+    for (int i = 1; i <= nn; i++) {
+        rs[step[1][i]] = i;
+    }
+    for (int x = 1; x <= nn; x++) {
+        printf("%d ", rs[x]);
+    }
+    printf("\n");
+    printf("Total cost %d\n", calculate(rs));
+    string x = "";
+    for (int i = 1; i <= nn; i++) {
+        x += " " + to_string(rs[i]);
+    }
+    return x;
 }
 int main() {
     int t;
