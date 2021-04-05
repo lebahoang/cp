88cf72f6569a43a0bbd5d2eb3e24f6020f98be34
lebahoang <hoangleba@gmail.com>
2021-04-06 02:06:02 +0700

    Add

--- codechef/Apr2021Challenge/1.cpp
+++ codechef/Apr2021Challenge/1.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <fstream>
+#include <assert.h>
 #include <iostream>
 #include <algorithm>
 #include <cmath>
@@ -11,63 +12,76 @@
 #include <utility>
 
 using namespace std;
-int bs(int s, int e, vector<int>& prefix, int sv) {
-    int l = s;
-    int r = e;
+using ll = long long;
+ll calculateV(int mid, int i, int j, vector<vector<ll>>& prefix) {
+    int ii = i-mid+1;
+    int jj = j-mid+1;
+    ll a = 0;
+    if (ii > 0) a = prefix[ii-1][jj-1];
+    ll b = 0;
+    if (ii-1 >= 0) b = prefix[ii-1][j];
+    ll c = 0;
+    if (jj-1 >= 0) c= prefix[i][jj-1];
+    ll v = prefix[i][j]-b-c+a;
+    return v;
+}
+int bs(int i, int j, int n, int m, vector<vector<ll>>& prefix) {
+    int maxL = min(i+1, j+1);
+    int l = 1;
+    int r = maxL;
     while (l <= r) {
         int mid = (l+r)/2;
-        int v = prefix[mid]-sv;
-        if ((mid == s && v >= 0) || (prefix[mid-1]-sv < 0 && v >= 0)) {
-            return mid;
-        } else if (prefix[mid-1]-sv >= 0) {
-            r = mid-1;
-        } else {
+        ll v = calculateV(mid, i, j, prefix);
+        if (v >= 0 && mid+1 > maxL) return mid;
+        ll vprev = calculateV(mid+1, i, j, prefix);
+        if (v >= 0 && vprev < 0) return mid;
+        if (vprev >= 0) {
             l = mid+1;
+        } else {
+            r = mid-1;
         }
     }
     return -1;
 }
-int solve() {
-    int rs = 0;
+ll solve() {
+    ll rs = 0;
     int n,m,k;
     cin >> n >> m >> k;
     vector<vector<int>> a(n, vector<int>(m, 0));
-    vector<int> start(n, -1);
-    vector<vector<int>> prefix(n, vector<int>(m,0));
+    vector<vector<ll>> prefix(n, vector<ll>(m,0));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cin >> a[i][j];
             a[i][j] -= k;
-            if (a[i][j] >= 0 && start[i] == -1) {
-                start[i] = j;
-                rs += m-j + min(n-i-1, m-j-1);
+            int t = a[i][j];
+            bool f1 = false;
+            bool f2 = false;
+            if (i-1 >= 0) {
+                f1 = true;
+                t += prefix[i-1][j];
             }
-            if (j == 0) prefix[i][j] = a[i][j];
-            else prefix[i][j] = prefix[i][j-1] + a[i][j];
+            if (j-1 >= 0) {
+                f2 = true;
+                t += prefix[i][j-1];
+            }
+            if (f1 && f2) {
+                t -= prefix[i-1][j-1];
+            }
+            prefix[i][j] = t;
         }
     }
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < m; j++) {
+    //         printf("%d ", a[i][j]);
+    //     }
+    //     printf("\n");
+    // }
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            printf("%d ", a[i][j]);
-        }
-        printf("\n");
-    }
-    printf("rs %d\n", rs);
-    for (int i = 0; i < n; i++) {
-        printf("i %d, start[i] %d\n", i,  start[i]);
-        for (int j = 0; j < start[i]; j++) {
-            int ind = -1;
-            if (j > 0)
-                ind = bs(start[i], m-1, prefix[i], prefix[i][j-1]);
-            else
-                ind = bs(start[i], m-1, prefix[i], 0);
-            printf("i %d, ind %d\n", i, ind);
-            if (ind >= 0) {
-                int x = i + (ind-j);
-                if (x < n) {
-                    rs += min(m-ind, n-x);
-                }
-            }
+            if (a[i][j] < 0) continue;
+            int maxlen = bs(i, j, n, m, prefix);
+            // printf("i %d j %d ind(max Length) %d\n", i, j, maxlen);
+            rs += maxlen;
         }
     }
     return rs;
@@ -77,7 +91,7 @@ int main() {
     int t;
     cin >> t;
     for (int i = 0; i < t; i++) {
-        printf("%d\n", solve());
+        printf("%lld\n", solve());
     }
     return 0;
 }
--- codechef/Apr2021Challenge/idea.py
+++ codechef/Apr2021Challenge/idea.py
@@ -1,29 +1,15 @@
 import random
 
-n = 10
-m = 10
-arr = []
-for _ in range(20):
-    t = []
-    for i in range(m):
-        if i == 0:
-            t.append(random.randint(1, 100))
-        else:
-            x = t[i-1] + random.randint(1, 10)
-            t.append(x)
-    arr.append(t)
+t = 1
+for i in range(t):
+    print(i+1)
+    n = 1000
+    m = 1000
+    k = random.randint(0, 10**9)
+    print(n,m,k)
+    for _ in range(n):
+        t = []
+        for _ in range(m):
+            t.append(str(random.randint(0, 10**9)))
+        print(' '.join(t))
 
-k = random.randint(1, 100)
-for i in range(n):
-    for j in range(m):
-        arr[i][j] -= k
-sum = [[0 for j in range(m)] for i in range(n)]
-for i in range(n):
-    prefix = 0
-    for j in range(m):
-        arr[i][j] -= k
-        prefix += arr[i][j]
-        sum[i][j] += prefix + sum[i-1][j]
-
-for i in range(n):
-    print(sum[i])
