47c351ed42a7a7a3949d5b4383c5fbb246317bc1
hoang le <v.hoangle@vinbrain.net>
2021-04-28 18:16:18 +0700

    add

--- gcj/2021/1b/1.cpp
+++ gcj/2021/1b/1.cpp
@@ -11,46 +11,124 @@
 
 // g++ -std=c++17 -O2 -o s.out 1.cpp && ./s.out < x.txt
 using namespace std;
-bool check(int k, int a, int b, int cnt, vector<int>& mm, vector<vector<int>>& dp) {
-    vector<int> m(mm.begin(), mm.end());
-    vector<int> v(5005, 0);
-    queue<int> q;
-    q.push(k);
-    v[k] = 1;
-    while (cnt > 0 && !q.empty()) {
-        k = q.front();
-        q.pop();
-        int x = k-a;
-        int y = k-b;
-        // printf("kkkk %d, cnt %d, x %d y %d\n", k, cnt, x, y);
-        // for (int i = 1; i < (int)m.size(); i++) {
-        //     if (m[i] > 0) {
-        //         printf("i %d m[i] %d\n", i, m[i]);
-        //     }
-        // }
-        if (x > 0) {
-            if (m[x] > 0) {
-                m[x]--;
-                cnt--;
-            } else {
-
+// bool check(int k, int n, int a, int b, int cnt, vector<int>& u, vector<vector<int>>& dp) {
+//     vector<int> m(u.begin(), u.end());
+//     vector<int> obtain(25, 0);
+//     queue<int> q;
+//     q.push(k);
+//     while (!q.empty()) {
+//         k = q.front();
+//         q.pop();
+//         int x = k-a;
+//         int y = k-b;
+//         // printf("kkkk %d, cnt %d, x %d y %d\n", k, cnt, x, y);
+//         // for (int i = 1; i < (int)m.size(); i++) {
+//         //     if (m[i] > 0) {
+//         //         printf("i %d m[i] %d\n", i, m[i]);
+//         //     }
+//         // }
+//         if (x > 0) {
+//             if (m[x] > 0) {
+//                 obtain[x]++;
+//                 m[x]--;
+//                 cnt--;
+//             } else {
+//                 vector<int> t = dp[x];
+//                 for (int i = 1; i <= n; i++) {
+//                     if (m[i] > 0) {
+//                         obtain[i] += t[i];
+//                         m[i] -= t[i];
+//                         cnt -= t[i];
+//                     }
+//                 }
+//             }
+//         }
+//         if (y > 0) {
+//             if (m[y] > 0) {
+//                 obtain[y]++;
+//                 m[y]--;
+//                 cnt--;
+//             } else {
+//                 vector<int> t = dp[y];
+//                 for (int i = 1; i <= n; i++) {
+//                     if (m[i] > 0) {
+//                         obtain[i] += t[i];
+//                         m[i] -= t[i];
+//                         cnt -= t[i];
+//                     }
+//                 }
+//             }
+//         }
+//         // printf("-----After: cnt %d\n", cnt);
+//         // for (int i = 1; i < (int)m.size(); i++) {
+//         //     if (m[i] > 0) {
+//         //         printf("i %d m[i] %d\n", i, m[i]);
+//         //     }
+//         // }
+//     }
+//     dp[k] = obtain;
+//     if (cnt == 0) return true;
+//     return false;
+// }
+bool check(int k, int n, int a, int b, int cnt, vector<int>& u, vector<int>& visited, vector<vector<int>>& dp) {
+    vector<int> m(u.begin(), u.end());
+    vector<int> obtain(25, 0);
+    int x = k-a;
+    int y = k-b;
+    // printf("kkkk %d, cnt %d, x %d y %d\n", k, cnt, x, y);
+    // for (int i = 1; i < (int)m.size(); i++) {
+    //     if (m[i] > 0) {
+    //         printf("i %d m[i] %d\n", i, m[i]);
+    //     }
+    // }
+    if (x > 0) {
+        if (x <= n && m[x] > 0) {
+            obtain[x]++;
+            m[x]--;
+            cnt--;
+        } else {
+            if (visited[x] == 0) {
+                check(x, n, a, b, cnt, m, visited, dp);
+            }
+            vector<int> t = dp[x];
+            for (int i = 1; i <= n; i++) {
+                obtain[i] += t[i];
+                m[i] -= t[i];
+                cnt -= t[i];
             }
         }
-        if (y > 0) {
-            if (m[y] > 0) {
-                m[y]--;
-                cnt--;
-            } else {
-                
-            }
-        }
-        // printf("-----After: cnt %d\n", cnt);
-        // for (int i = 1; i < (int)m.size(); i++) {
-        //     if (m[i] > 0) {
-        //         printf("i %d m[i] %d\n", i, m[i]);
-        //     }
-        // }
     }
+    if (y > 0) {
+        if (y <= n && m[y] > 0) {
+            obtain[y]++;
+            m[y]--;
+            cnt--;
+        } else {
+            if (visited[y] == 0) {
+                check(y, n, a, b, cnt, m, visited, dp);
+            }
+            vector<int> t = dp[y];
+            for (int i = 1; i <= n; i++) {
+                obtain[i] += t[i];
+                m[i] -= t[i];
+                cnt -= t[i];
+            }
+        }
+    }
+    // printf("-----After: cnt %d\n", cnt);
+    // for (int i = 1; i < (int)m.size(); i++) {
+    //     if (m[i] > 0) {
+    //         printf("i %d m[i] %d\n", i, m[i]);
+    //     }
+    // }
+    printf("%d, cnt %d, ", k, cnt);
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", obtain[i]);
+    }
+    printf("\n");
+
+    visited[k] = 1;
+    dp[k] = obtain;
     if (cnt == 0) return true;
     return false;
 }
@@ -59,11 +137,16 @@ int solve(int n, int a, int b, vector<int>& u) {
     for (int i = 1; i <= n; i++) {
         cnt += u[i];
     }
-    int max_k = 12;
-    vector<vector<int>> dp(max_k, vector<int>(25, 0));
+    int max_k = 20;
     for (int k = 0; k < max_k; k++) {
-        
+        printf("k %d\n", k);
+        vector<vector<int>> dp(max_k, vector<int>(25, 0));
+        vector<int> visited(max_k, 0);
+        if (check(k, n, a, b, cnt, u, visited, dp)) {
+            return k;
+        }
     }
+    // printf("dsadas\n");
     return -1;
 }
 int main() {
