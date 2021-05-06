f69f1fd55ab43be2e5ca2a82f34542d7d4ce4448
lebahoang <hoangleba@gmail.com>
2021-05-07 02:00:42 +0700

    add

--- gcj/2021/1c/3.cpp
+++ gcj/2021/1c/3.cpp
@@ -11,6 +11,7 @@
 
 // g++ -std=c++17 -Wshadow -Wall -o s.out -O2 -Wno-unused-result -g -fsanitize=address -fsanitize=undefined 3.cpp && ./s.out < x.txt
 using namespace std;
+const int INF = 1e9+2;
 void vector2string(vector<int> a) {
     string x = "";
     for(int i: a) {
@@ -27,24 +28,36 @@ void get(string& arr, vector<int>& x, vector<int>& y) {
                 x.push_back(c);
             c = 1;
             cur = arr[i];
-            y.push_back(i);
+            y[i] = 1;
         } else {
             c++;
         }
     }
     x.push_back(c);
 }
-bool check(int j, string& a, string& b) {
-    int bz = (int)b.size();
-    if (bz-j > (int)a.size()) return false;
-    if (j > 0 && a[j-1] == a[j]) return false;
-    int i = (int)a.size()-1;
-    while (j < bz) {
-        
-    }
+bool check(int i, int j, string& a, string& b) {
+    int az = (int)a.length();
+    int bz = (int)b.length();
+    if (i )
 }
-int solve(string& a, string& b, vector<int>& x1, vector<int>& y1, vector<int>& x2, vector<int>& y2) {
-    for (int j = (int)b.size()-1; j >= 0; j--) {
+int solve(string& a, string& b, vector<int>& x1, vector<int>& y1, vector<int>& x2) {
+    int rs = INF;
+    int az = (int)a.length();
+    int bz = (int)b.length();
+    if ((int)x2.size() == 1) {
+        if ((int)x1.size() == 1) {
+            if (a[0] == b[0]) {
+                return 0;
+            }
+            return 1;
+        }
+        if (b[0] == '0') {
+            return (int)x1.size();
+        }
+    }
+    
+    for (int j = 0; j < bz; j++) {
+        int i = az-(bz-j+1)+1;
 
     }
 }
@@ -57,12 +70,12 @@ int main() {
         cin >> a >> b;
         printf("%s %s\n", a.c_str(), b.c_str());
         vector<int> x1 = {};
-        vector<int> y1 = {};
+        vector<int> y1((int)a.length(), -1);
         get(a, x1, y1);
         vector2string(x1);
         vector2string(y1);
         vector<int> x2 = {};
-        vector<int> y2 = {};
+        vector<int> y2((int)b.length(), -1);
         get(b, x2, y2);
         vector2string(x2);
         vector2string(y2);
