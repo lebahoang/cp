b948cc409317dd00b3a39cead3494f93ff6689da
lebahoang <hoangleba@gmail.com>
2021-10-30 22:13:23 +0700

    update

--- leetcode/1872.cpp
+++ leetcode/1872.cpp
@@ -16,26 +16,24 @@ using namespace std;
 using ll = long long;
 int solve(vector<int>& st) {
     int n = (int)st.size();
-    vector<vector<int>> dp(n, vector<int>(2, 0));
+    vector<int> dp(n, 0);
     vector<int> prefix(n, 0);
     for (int i = 0; i < n; i++) {
         if (i == 0) prefix[i] = st[i];
         else prefix[i] = prefix[i-1]+st[i];
     }
-    dp[1][0] = prefix[1];
-    dp[1][1] = 1e9+9;
+    dp[1] = prefix[1];
     for (int i = 2; i < n; i++) {
-        if (dp[i-1][1] < 1e9) {
-            dp[i][0] = max(dp[i-1][0]+st[i], dp[i-1][1]+prefix[i]);
-        } else {
-            dp[i][0] = dp[i-1][0]+st[i];
+        dp[i] = -1e9-9;
+        for (int j = i-1; j >= 1; j--) {
+            int t = prefix[i]-prefix[j];
+            dp[i] = max(dp[i], prefix[j]-dp[j]+t);
+            printf("i %d j %d, %d\n", i, j, prefix[j]-dp[j]+t);
+            dp[i] = max(dp[i], t);
         }
-            
-        dp[i][1] = min(dp[i-1][1]-st[i], dp[i-1][0]-prefix[i]);
-        dp[i][0] = max(dp[i][0], prefix[i]);
     }
-    for(int i = 1; i < n; i++) printf("dp[%d][0] %d, dp[%d][1] %d\n", i, dp[i][0], i, dp[i][1]);
-    return max(dp[n-1][0], dp[n-1][1]);
+    for(int i = 1; i < n; i++) printf("dp[%d] %d, %d\n", i, dp[i], prefix[i]-dp[i]);
+    return dp[n-1];
 }
 int main() {
     vector<int> st = {-1,2,-3,4,-5};
