69100e9ce8ef1bdb17aff92ef89ce3093e26655a
hoang le ba <hoangleba@gmail.com>
2021-11-21 16:07:06 +0700

    add

--- codeforces/1800/1556C.cpp
+++ codeforces/1800/1556C.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-void iterate_from(int i, int n, vector<long long>& a, vector<vector<bool>>& mem) {
+void iterate_from(int i, int n, vector<long long>& a, vector<vector<bool>>& mem, vector<long long>& b1, vector<long long>& b2) {
     stack<long long> s;
     s.push(a[i]);
     int j = i+1;
@@ -34,6 +34,8 @@ void iterate_from(int i, int n, vector<long long>& a, vector<vector<bool>>& mem)
             }
             if (s.empty() && neg == 0) {
                 mem[i][j] = true;
+                b1[i] = max(b1[i], (long long)(j-i+1)/2);
+                b2[j] = max(b2[j], (long long)(j-i+1)/2);
             } else if (s.empty() && neg > 0) {
                 break;
             }
@@ -49,38 +51,82 @@ int main() {
         int n;
         cin >> n;
         vector<long long> a(n+2, 0);
+        vector<long long> b1(n+2, 0);
+        vector<long long> b2(n+2, 0);
         vector<vector<bool>> mem(n+2, vector<bool>(n+2, false));
         for (int i= 1; i <= n; i++) cin >> a[i];
         for (int i= 1; i <= n; i++) {
             if (i%2 == 1) {
-                iterate_from(i, n, a, mem);
-                for (int j = i+1; j <= n; j++) {
-                    if (j%2 == 0) {
-                        if (mem[i][j])
-                            printf("mem[%d][%d]: true\n", i, j);
-                        else
-                            printf("mem[%d][%d]: false\n", i, j);
-                    }
-                }
+                iterate_from(i, n, a, mem, b1, b2);
+                // for (int j = i+1; j <= n; j++) {
+                //     if (j%2 == 0) {
+                //         if (mem[i][j])
+                //             printf("mem[%d][%d]: true\n", i, j);
+                //         else
+                //             printf("mem[%d][%d]: false\n", i, j);
+                //     }
+                // }
             }
         }
+        // for (int i = 1; i <= n; i += 2) {
+        //     for (int j = i+1; j <= n; j += 2) {
+        //         if (j >= i+3) {
+        //             if (a[i] >= a[i+1] && a[j] >= a[j-1]) {
+        //                 long long x = min(a[i], a[i+1]);
+        //                 long long y = min(a[j-1], a[j]);
+        //                 long long z = min(a[i]-x, a[j]-y);
+        //                 if (j == i+3 || mem[i+2][j-2]) {
+        //                     rs += z+1;
+        //                 }
+        //             }
+        //             // if (!mem[i][j] && a[i] >= a[i+1] && (j == i+3 || mem[i+2][j-2])) rs++;
+        //             // if (!mem[i][j] && a[j] >= a[j-1] && (j == i+3 || mem[i+2][j-2])) rs++;
+        //         } else {
+        //             long long x = min(a[i], a[j]);
+        //             rs += x;
+        //         }
+        //         // printf("I %d j %d RS %lld\n", i, j, rs);
+        //     }
+        // }
         for (int i = 1; i <= n; i += 2) {
             for (int j = i+1; j <= n; j += 2) {
-                if (j >= i+3) {
-                    if (a[i] >= a[i+1] && a[j] >= a[j-1]) {
-                        long long x = min(a[i], a[i+1]);
-                        long long y = min(a[j-1], a[j]);
-                        long long z = min(a[i]-x, a[j]-y);
-                        if (j == i+3 || mem[i+2][j-2]) {
-                            rs += z+1;
+                if (mem[i][j]) {
+                    if (j == i+1) {
+                        rs += a[j];
+                    } else {
+                        rs++;
+                    }
+                        
+                    if (i-2 >= 0 && a[i-2] > a[i-1]) {
+                        rs++;
+                    }
+                    if (j+2 <= n && a[j+2] > a[j+1]) {
+                        rs++;
+                    }
+                    if (i-2 >= 0 && a[i-2] > a[i-1] && j+2 <= n && a[j+2] > a[j+1]) {
+                        long long x = a[i-2]-a[i-1];
+                        long long y = a[j+2]-a[j+1];
+                        long long z = min(x,y);
+                        rs += z+1;
+                        if (x < y) {
+                            if (i-3 >= 0 && b2[i-3] > 0) {
+                                rs += b2[i-3];
+                            } else if (i-4 >= 0 && a[i-4] > a[i-3]) {
+                                rs++;
+                            }
+                        } else {
+                            if (j+3 <= n && b1[j+3] > 0) {
+                                rs += b1[j+3];
+                            } else if (j+4 <= n && a[j+4] > a[j+3]) {
+                                rs++;
+                            }
                         }
                     }
-                    // if (!mem[i+2][j-2] && mem[i][j]) rs++;
-                } else {
-                    long long x = min(a[i], a[j]);
-                    rs += x;
+                } else if (j == i+1) {
+                    rs += min(a[i], a[j]);
+                    if (a[i] > a[j] && i+3 <= n && a[i+3] > a[i+2]) rs++;
                 }
-                // printf("I %d j %d RS %lld\n", i, j, rs);
+                printf("I %d J %d, rs %lld\n", i, j, rs);   
             }
         }
         printf("%lld\n", rs);
