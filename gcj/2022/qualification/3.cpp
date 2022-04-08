c769339ff7bcb433c8b178f3818de7687b7aae7b
hoang le ba <hoangleba@gmail.com>
2022-04-08 12:58:18 +0700

    add

--- gcj/2022/qualification/3.cpp
+++ gcj/2022/qualification/3.cpp
@@ -10,20 +10,50 @@
 #include <map>
 #include <set>
 using namespace std;
+using ll = long long;
 const int mod = 1e9+7;
-// g++ -std=c++17 -Wshadow -Wall -o s.out -O2 -Wno-unused-result -g -fsanitize=address -fsanitize=undefined 2.cpp && ./s.out
+// g++ -std=c++17 -Wshadow -Wall -o s.out -O2 -Wno-unused-result -g -fsanitize=address -fsanitize=undefined 3.cpp && ./s.out
 
 struct F {
-    int s;
-    int val;
+    ll s;
+    ll val;
 };
+
+F dfs(int inode, vector<vector<int>>& nei, vector<ll>& f) {
+    int n = nei[inode].size();
+    if (n == 0) {
+        // printf("INode %d, sum %lld val %lld\n", inode, (ll)0, f[inode]);
+        return F{0, f[inode]};
+    }
+    
+    ll totalSum = 0;
+    ll totalV = 0;
+    ll v = -1;
+    for (int i = 0; i < n; i++) {
+        F t = dfs(nei[inode][i], nei, f);
+        if (v == -1 || v > t.val) {
+            v = t.val;
+        }
+        totalSum += t.s;
+        totalV += t.val;
+    }
+    ll sum = 0;
+    if (n == 1) {
+        sum = totalSum;
+    } else {
+        sum = totalSum + totalV - v;
+    }
+    ll inodeV = max(f[inode], v);
+    // printf("INode %d, sum %lld val %lld\n", inode, sum, inodeV);
+    return F{sum, inodeV};
+}
 int main() {
     int t;
     cin >> t;
     for (int cs = 1; cs <= t; cs++) {
         int n;
         cin >> n;
-        vector<int> f(n+5, 0);
+        vector<ll> f(n+5, 0);
         vector<vector<int>> nei(n+5, vector<int>{});
         for (int i = 1; i <= n; i++) cin >> f[i];
         int root = 0;
@@ -32,6 +62,8 @@ int main() {
             cin >> x;
             nei[x].push_back(i);
         }
+        F rs = dfs(root, nei, f);
+        printf("Case #%d: %lld\n", cs, rs.s + rs.val);
     }
     return 0;
 }
