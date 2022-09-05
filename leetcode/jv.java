187cf0d3edbbc4f4be92b3283ac17f9439ee3e2b
lebahoang <hoangleba@gmail.com>
2022-09-05 14:27:34 +0700

    update

--- leetcode/jv.java
+++ leetcode/jv.java
@@ -1,68 +1,34 @@
 import java.util.ArrayList;
-// javac -cp ".;*" jv.java  && java -Xmx512M -Xss64M -DONLINE_JUDGE=true -Duser.language=en -Duser.region=US -Duser.variant=US Main
-interface OrderPaymentInterface {
-    public int getPaidMoney(Order order);
-}
-abstract class PromotionAbstract implements OrderPaymentInterface {
-    private OrderPayment orderPayment;
-    public PromotionAbstract(OrderPayment orderPayment) {
-        this.orderPayment = orderPayment;
-    }
-    public int getPaidMoney(Order order) {
-        return this.orderPayment.getPaidMoney(order);
-    }
-}
-class Item {
-    private int price;
-    public Item(int price) {
-        this.price = price;
-    }
-    public int getPaidMoney(int amount) {
-        return amount * price;
-    }
-}
-class Order {
-    private Item[] items;
-    public Order(Item[] items) {
-        this.items = items;
-    }
-    public Item[] getItems() {
-        return this.items;
-    }
-}
-class OrderPayment implements OrderPaymentInterface {
-    public int getPaidMoney(Order order) {
-        int total = 0;
-        for (Item x : order.getItems()) {
-            total += x.getPaidMoney(10);
-        }
-        return total;
-    }
-}
+import java.util.Arrays;
+import java.util.List;
 
-class PromotionByAmountOfItem extends PromotionAbstract {
-    public PromotionByAmountOfItem(OrderPayment orderPayment) {
-        super(orderPayment);
-    }
-    public int getPaidMoney(Order order) {
-        int total = super.getPaidMoney(order);
-        return total - this.getDisCount(order);
-    }
-    private int getDisCount(Order order) {
-        return 100;
-    }
-}
-
+// javac -cp ".;*" jv.java  && java -Xmx512M -Xss64M Main
 class Main {
-    public void run() {
-        Item[] i = {new Item(100)};
-        Order order = new Order(i);
-        OrderPayment payment = new OrderPayment();
-        PromotionByAmountOfItem p = new PromotionByAmountOfItem(payment);
-        System.out.println(p.getPaidMoney(order));
-    }
     public static void main(String[] args) {
-        Main m = new Main();
-        m.run();
+        String x = "fdaf{a,b,c}dsada{x,y,z}";
+        char[] y = x.toCharArray();
+        StringBuilder cur = new StringBuilder();
+        List<List<String>> rs = new ArrayList<List<String>>();
+        for (int i = 0; i < y.length; i++) {
+            if (y[i] != '{' && y[i] != '}') {
+                cur.append(String.valueOf(y[i]));
+            } else if (y[i] == '{') {
+                if(cur.length() > 0) rs.add(Arrays.asList(cur.toString()));
+                cur.setLength(0);
+            } else if (y[i] == '}') {
+                if (cur.length() > 0) rs.add(Arrays.asList(cur.toString().split(",")));
+                cur.setLength(0);
+            }
+        }
+        if(cur.length() > 0) rs.add(Arrays.asList(cur.toString()));
+        System.out.println(rs);
+        String[] t = rs.get(1).toArray(new String[0]);
+        System.out.println(Arrays.toString(t));
+
+        // List<List<Integer>> q = new ArrayList<>();
+        // int n = 100;
+        // for (int i = 0; i < n; i++) q.add(new ArrayList<>());
+        
+
     }
 }
